71ac3641ce90509c7ce856fef1641771b227dbaf
Lunagariya Raj <lunagariyarajy2093@gmail.com>
2026-06-28 12:03:38 +0530

    Create task and manage task pages completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import WebRoutes from './routes/WebRoutes'
 import { useApi } from './context/ApiProvider'
+import { Toaster } from 'react-hot-toast';
 
 export default function App() {
   const { loading } = useApi();
@@ -9,6 +10,14 @@ export default function App() {
     <div>
       { loading && <>Loading ...</> }
       { !loading && <WebRoutes/> }
+      <Toaster
+        toastOptions={{
+          className: "", 
+          style: {
+            fontSize: "13px",
+          }
+        }}
+      />
     </div>
   )
 }
--- client/src/pages/admin/CreateTask.jsx
+++ client/src/pages/admin/CreateTask.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../layouts/DashboardLayout";
 import { PRIORITY_DATA } from "../../utils/data";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -6,11 +6,18 @@ import { LuTrash2 } from "react-icons/lu";
 import SelectDropdown from "../../components/inputs/SelectDropdown";
 import SelectUsers from "../../components/inputs/SelectUsers";
 import TodoListInput from "../../components/inputs/TodoListInput";
+import AddAttachmentsInput from "../../components/inputs/AddAttachmentsInput";
+import { useApi } from "../../context/ApiProvider";
+import { API_PATHS } from "../../utils/apiPaths";
+import toast from "react-hot-toast";
+import moment from "moment";
+import Modal from "../../components/Modal";
 
 export default function CreateTask() {
   const location = useLocation();
   const { taskId } = location.state || {};
   const navigate = useNavigate();
+  const { api } = useApi();
 
   const [taskData, setTaskData] = useState({
     title: "",
@@ -41,15 +48,147 @@ export default function CreateTask() {
     });
   };
 
-  const createTask = async () => {};
+  const createTask = async () => {
+    setLoading(true);
 
-  const updateTask = async () => {};
+    try {
+      const todolist = taskData.todoChecklist?.map((item) => ({
+        text: item,
+        completed: false,
+      }));
 
-  const handleSubmit = async () => {};
+      const response = await api.post(API_PATHS.TASKS.CREATE_TASK, {
+        ...taskData,
+        dueDate: new Date(taskData.dueDate).toISOString(),
+        todoChecklist: todolist,
+      });
 
-  const getTaskDetailsByID = async () => {};
+      toast.success("Task created successfully");
+      clearData();
 
-  const deleteTask = async () => {};
+    } catch(error) {
+      console.error(`Error at createTask: ${error}`);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updateTask = async () => {
+    setLoading(true);
+
+    try {
+      const todolist = taskData.todoChecklist?.map((item) => {
+        const prevTodoChecklist = currentTask?.todoChecklist || [];
+        const matchedTask = prevTodoChecklist.find((task) => task.text == item);
+
+        return {
+          text: item,
+          completed: matchedTask ? matchedTask.completed : false,
+        };
+      });
+
+      const response = await api.put(
+        API_PATHS.TASKS.UPDATE_TASK(taskId),
+        {
+          ...taskData,
+          dueDate: new Date(taskData.dueDate).toISOString(),
+          todoChecklist: todolist,
+        }
+      );
+
+      toast.success("Task Updated Successfully");
+    } catch(error) {
+      console.error(`Error at updateTask: ${error}`);
+      setLoading(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSubmit = async () => {
+    setError(null);
+
+    // Input validation
+    for (const [key, value] of Object.entries(taskData)) {
+      if (["title", "description"].includes(key)) {
+        if (!value.trim()) {
+          setError(`${key} is required`);
+          return;
+        }
+      }
+      else if (key == "dueDate") {
+        if (!value) {
+          setError(`${key} is required`);
+          return;
+        }
+      }
+      else if (["assignedTo", "todoChecklist"].includes(key)) {
+        if (value.length === 0) {
+          const msg = 
+          key == "assignedTo" ? "Task not assigned to any member" 
+          : "Add atleast one todo task";
+          setError(msg);
+          return;
+        }
+      }
+    }
+
+    if (taskId) {
+      updateTask();
+      return;
+    }
+
+    createTask();
+  };
+
+  const getTaskDetailsByID = async () => {
+    try {
+      const response = await api.get(
+        API_PATHS.TASKS.GET_TASK_BY_ID(taskId)
+      );
+
+      if(response.data) {
+        const taskInfo = response?.data?.data?.task;
+        setCurrentTask(taskInfo);
+
+        setTaskData((prevState) => ({
+          title: taskInfo.title,
+          description: taskInfo.description,
+          priority: taskInfo.priority,
+          dueDate: taskInfo.dueDate
+            ? moment(taskInfo.dueDate).format("YYYY-MM-DD")
+            : null,
+          assignedTo: taskInfo?.assignedTo?.map((item) => item?._id) || [],
+          todoChecklist:
+            taskInfo?.todoChecklist?.map((item) => item?.text) || [],
+          attachments: taskInfo?.attachments || [],
+        }));
+      }
+    } catch(error) {
+      console.log(`Error at getTaskDetailsByID: ${error}`);
+    }
+  };
+
+  const deleteTask = async () => {
+    try {
+      await api.delete(API_PATHS.TASKS.DELETE_TASK(taskId));
+
+      setOpenDeleteAlert(false);
+      toast.success("Task deleted successfully");
+      navigate("/admin/tasks");
+    } catch (error) {
+      console.error(`Error at deleteTask: ${error}, error message: ${error.response?.data?.message || error.message}`);
+    }
+  };
+
+  useEffect(() => {
+    if(taskId) {
+      getTaskDetailsByID(taskId);
+    }
+
+    return () => {};
+  }, [taskId])
 
   return (
     <DashboardLayout activeMenu={"Create Task"}>
@@ -165,14 +304,41 @@ export default function CreateTask() {
               />
             </div>
 
-            <div className="">
-              <label htmlFor="" className="">Add Attachments</label>
+            <div className="mt-3">
+              <label htmlFor="" className="text-xs font-medium text-slate-600">Add Attachments</label>
+              <AddAttachmentsInput 
+                attachments={taskData?.attachments}
+                setAttachments={(value) => handleValueChange("attachments", value)}
+              />
+            </div>
 
-              
+            {error && (
+              <p className="text-xs font-medium text-red-500 mt-5">{error}</p>
+            )}
+
+            <div className="flex justify-end mt-7">
+              <button 
+                className="add-btn"
+                onClick={handleSubmit}
+                disabled={loading}
+              >
+                {taskId ? "UPDATE TASK" : "CREATE TASK"}
+              </button>
             </div>
           </div>
         </div>
       </div>
+
+      <Modal
+        isOpen={openDeleteAlert}
+        onClose={() => setOpenDeleteAlert(false)}
+        title={"Delete Task"}
+      >
+        <DeleteAlert
+          content="Are you sure you want to delete this task?"
+          onDelete={() => deleteTask()}
+        />
+      </Modal>
     </DashboardLayout>
   );
 }
