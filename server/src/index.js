c0f8f3b76408688d4e73dc4588e14a2ceedbc9e1
Lunagariya Raj <lunagariyarajy2093@gmail.com>
2026-06-29 16:55:06 +0530

    Changes for render deployment

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ import { connectDB } from "./db/db.js";
 const PORT = process.env.PORT || 8000;
 
 connectDB().then((val) => {
-    app.listen(PORT, ()=>{
+    app.listen(PORT, '0.0.0.0', ()=>{
         console.log(`Server listening at PORT: ${PORT}`);
         console.log(`---------------------------------------------------------------------------`);
     })
